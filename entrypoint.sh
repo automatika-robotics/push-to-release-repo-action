0f2dab4b897adc5177256dc6014340f0c791d1ec
ahr <ahrasheed@gmail.com>
2024-11-29 10:21:40 +0100

    (feature) Adds pushing a specific folder from the source repo to the destination

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,10 @@ DESTINATION_USERNAME="${1}"
 ACCESS_TOKEN="${2}"
 DESTINATION_REPOSITORY="${3}"
 GIT_SERVER="${4}"
-TARGET_BRANCH="${5}"
+SOURCE_FOLDER=${5}
+TARGET_BRANCH="${6}"
+COMMIT_EMAIL="${7:-$DESTINATION_USERNAME@$GIT_SERVER}"
+COMMIT_MESSAGE="${8}"
 
 DESTINATION_URL="https://$DESTINATION_USERNAME:$ACCESS_TOKEN@$GIT_SERVER/$DESTINATION_REPOSITORY.git"
 
@@ -35,8 +38,55 @@ else
     exit 1
 fi
 
+
+# Check if source folder exists in repo
+if [ -d $SOURCE_FOLDER ]
+then
+    echo '[+] Found source folder: '$SOURCE_FOLDER
+else
+    echo "::error:: Source folder not found in the repository"
+    echo "::error:: Make sure you have checked out the correct branch of the source repository using the checkout action as follows:"
+    echo "::error:: actions/checkout@v4"
+    echo "::error:: with:"
+    echo "::error::   ref: branch-name"
+    exit 1
+fi
+
 echo "[+] Applying github specific fix to push to external repos"
 git config --unset-all http.https://github.com/.extraheader
 
-echo "[+] Pushing to target"
-git push --no-thin --follow-tags $DESTINATION_URL --set-upstream $TARGET_BRANCH
+if [ "$SOURCE_FOLDER" != "./" ]
+then
+    # Clone the destination repository into a temporary directory
+    CLONE_DIR=$(mktemp -d)
+    echo "[+] Cloning destination repo to: $CLONE_DIR"
+    git clone $DESTINATION_URL $CLONE_DIR
+
+    # Remove the target folder in the cloned repository if it exists
+    rm -rf "$CLONE_DIR/$SOURCE_FOLDER"
+
+    # Copy the source folder into the cloned repository
+    cp -r "$SOURCE_FOLDER" "$CLONE_DIR/"
+
+    echo "[+] Copied source folder to destination repo."
+
+    # Navigate to the cloned directory
+    cd $CLONE_DIR
+
+    # Setup git user and email for commit
+    git config user.name "$DESTINATION_USERNAME"
+    git config user.email "$COMMIT_EMAIL"
+
+    # Add changes and commit
+    echo "[+] Committing changes to destination repo."
+    git add .
+    git commit -m "${COMMIT_MESSAGE}"
+
+    # Push changes to the target branch
+    git push origin --set-upstream $TARGET_BRANCH
+    echo "[+] Pushed changes to: $DESTINATION_URL on branch: $TARGET_BRANCH"
+
+else
+    echo "[+] Pushing to target"
+    git push --no-thin --follow-tags $DESTINATION_URL --set-upstream $TARGET_BRANCH
+fi
